f79621ed9668be0e54abb160e52733bcddaa5bb7
mofan <524662994@qq.com>
2021-01-05 17:49:22 +0800

    1

---
+++ zuul/src/main/java/com/erp/zuul/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.erp.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages="com.erp")
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
